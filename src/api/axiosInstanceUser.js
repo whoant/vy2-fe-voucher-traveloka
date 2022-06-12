8f3b8bb96405be7dd8b8b60b0d4de8c816f54946
whoant <vovanhoangtuan4.2@gmail.com>
2022-06-12 23:13:10 +0700

    Create axios instance user

---
+++ src/api/axiosInstanceUser.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: process.env.REACT_APP_END_POINT,
+});
+
+instance.interceptors.request.use(function (config) {
+    const storageUser = localStorage.getItem('user');
+
+    if (storageUser) {
+        const { userId, token } = JSON.parse(storageUser);
+        config.headers = {
+            ...config.headers,
+            'user_id': userId,
+            Authorization: `Bearer ${token}`
+        };
+    }
+
+    return config;
+}, function (error) {
+
+    return Promise.reject(error);
+});
+
+// Add a response interceptor
+instance.interceptors.response.use(function (response) {
+
+    return response;
+}, function (error) {
+
+    return Promise.reject(error);
+});
+
+export default instance;
