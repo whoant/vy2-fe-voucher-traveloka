3e60df5f7436610a912cd7321c7d3018f3f60879
whoant <vovanhoangtuan4.2@gmail.com>
2022-06-14 09:09:15 +0700

    Add analyze chart

--- src/pages/Partner/Partner.jsx
+++ src/pages/Partner/Partner.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ChartStistic from '../../components/Chart/ChartSatistic';
-import TableDetails from '../../components/TableDetails/TableDetails';
+import VoucherPartnerApi from "../../api/voucherPartner.api";
 
 const Partner = () => {
-    const categories = ['T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'T8', 'T9', 'T10', 'T11', 'T12'];
-    const series = [{
-        name: 'Doanh thu',
-        data: [30, 40, 25, 50, 49, 21, 70, 51, 49, 21, 70, 51]
-    }, {
-        name: 'Lợi nhuận',
-        data: [23, 12, 54, 61, 32, 56, 81, 19, 54, 61, 32, 56]
-    }, {
-        name: 'Hoa hồng',
-        data: [26, 23, 43, 21, 12, 31, 23, 43, 51, 34, 14, 23]
-    }];
+    const [categories, setCategories] = useState([]);
+    const [series, setSeries] = useState([]);
+
+    useEffect(() => {
+        const getAnalyzeData = async () => {
+            try {
+                const { data } = await VoucherPartnerApi.getAnalyzeData();
+                setCategories(data.data.months);
+                setSeries(data.data.series);
+            } catch (e) {
+
+            }
+        }
+
+        getAnalyzeData();
+    }, []);
+
+    /*
+        const categories = ['T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'T8', 'T9', 'T10', 'T11', 'T12'];
+        const series = [{
+            name: 'Doanh thu',
+            data: [30, 40, 25, 50, 49, 21, 70, 51, 49, 21, 70, 51]
+        }];
+
+     */
 
     return (
         <div className="row">
