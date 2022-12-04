bd7cb09329bfa9261e2279332f2395d0c5d38185
LeVanHieu0509 <LeVanHieu0509@gmail.com>
2022-12-04 10:38:10 +0700

    chore/testDemoTool

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,9 @@
 const VARS = {
-    PAYPAL_CLIENT_ID: 'ASubwLfr_x30DhkxZ8iWfHG-ipJPEtzJA5BmWhf5rxaOm3yZBgJYQYBMoRa4W_0UVrDECHTU_Go3e_-k',
-    API_ENDPOINT: 'http://94.100.26.30:30002',
-    APP_URL: 'http://94.100.26.30:30003'
+  PAYPAL_CLIENT_ID:
+    "ASubwLfr_x30DhkxZ8iWfHG-ipJPEtzJA5BmWhf5rxaOm3yZBgJYQYBMoRa4W_0UVrDECHTU_Go3e_-k",
+  API_ENDPOINT: "http://94.100.26.30:30002",
+  // APP_URL: 'http://94.100.26.30:30003'
+  APP_URL: "http://localhost:3000",
 };
 
 export default { VARS };
